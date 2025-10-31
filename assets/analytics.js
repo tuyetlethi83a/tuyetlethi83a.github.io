d3def31c46fa3769bfb8868470954f1822a5d7e4
tuyetlethi83a <nguyenthinghiemlienson@gmail.com>
2025-10-31 20:12:03 +0700

    feat(seo): GA4 config (tuyetlethi)

--- assets/analytics.js
+++ assets/analytics.js
@@ -1,8 +1,5 @@
-(function(w,d,s,src,i){
-  w.dataLayer=w.dataLayer||[];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', i);
-  var j=d.createElement(s); j.async=true; j.defer=true; j.src=src+i;
-  var f=d.getElementsByTagName(s)[0]; f.parentNode.insertBefore(j,f);
-})(window,document,'script','https://www.googletagmanager.com/gtag/js?id=','G-6WLTPVXXG6');
+// REPLACE WHOLE FILE: /assets/analytics.js
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-6WLTPVXXG6'); // GA4 - tuyetlethi
